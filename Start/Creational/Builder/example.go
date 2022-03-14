81f98a5270493700d118b3f9777f91819717f5a2
Severin Ferrand <19885877+Seburan@users.noreply.github.com>
2022-03-14 23:52:01 +0900

    builder pattern

--- Start/Creational/Builder/example.go
+++ Start/Creational/Builder/example.go
@@ -1,9 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 	// TODO: Create a NotificationBuilder and use it to set properties
+	var bldr = newNotificationBuilder();
 
 	// TODO: Use the builder to set some properties
+	bldr.SetTitle("New notification");
+	bldr.SetSubTitle("New subtitle");
+	bldr.SetIcon("icon.png");
+	bldr.SetImage("image.png");
+	bldr.SetPriority(5);
+	bldr.SetMessage("This is a basic notification with text");
+	bldr.SetType("Alert Type");
+
+	notif, err := bldr.Build();
+	if err != nil {
+		fmt.Println("Error creating the notification", err);
+	} else {
+		fmt.Printf("Notification :%+v\n", notif);
+	}
+
 
 	// TODO: Use the Build function to create a finished object
 
